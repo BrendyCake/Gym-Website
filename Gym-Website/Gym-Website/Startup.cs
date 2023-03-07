a0cf22d0455c74ebe9f60fbe4f50830cfbed8e8a
BrendyCake <64937205+BrendyCake@users.noreply.github.com>
2023-03-07 14:41:20 +1300

    Initial Commit

---
+++ Gym-Website/Gym-Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gym_Website.Startup))]
+namespace Gym_Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
